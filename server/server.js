d66cca075f54ae3988961f64bbd377983df496b5
mikemajesty <mikemajesty2011@gmail.com>
2016-08-06 17:59:09 -0300

    user create

--- server/server.js
+++ server/server.js
@@ -6,15 +6,25 @@ const serviceUser = require('./services/userService');
 
 app.use(parser());
 
-app.get('/user', (req, res) => {
-  const user = {login: "wwww", password: "wwww", name: "wwww"};
-  serviceUser.create(user);
+app.post('/user/create', (req, res) => {
+  const user = {login:req.body.login, password: req.body.password, name: req.body.name};
+      console.log(serviceUser.create(user));
+      res.json(201);
 });
 
 // app.post('/user', (req, res) => {
 //   res.json({nome: "fulano", idade:28});
 // });
 //
+
+app.post('/login', (req, res) => {
+  const userRequest = {login: req.body.login, password: req.body.password};
+  serviceUser.findByLogin(userRequest).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(500);
+  })
+});
 
 
 const PORT = process.env.port || 3000;
--- server/services/userService.js
+++ server/services/userService.js
@@ -2,12 +2,21 @@ const sequelize = require('../data/database').sequelize;
 const modelUser = require('../model/user');
 const create = (data) => {
   sequelize.sync().then(() => {
-     return modelUser.User.create(data);
-   }).then( (data) => {
-     console.log('entity was saved');
-   });
+     return modelUser.User.create(data).then( (data) => {
+       return data.get();
+     });;
+   })
+}
+
+const findByLogin = (user) => {
+  return modelUser.User.findOne({
+    where: {login: user.login, password: user.password}
+  }).then((data) => {
+    return data.get();
+  });
 }
 
 module.exports = {
-  create
+  create,
+  findByLogin
 }
