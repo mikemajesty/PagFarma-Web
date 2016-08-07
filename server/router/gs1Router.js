3dd49f7f05292ba5a2a0bd82294fdbd81a037b9e
mikemajesty <mikemajesty2011@gmail.com>
2016-08-06 23:44:24 -0300

    create api dto

--- server/router/gs1Router.js
+++ server/router/gs1Router.js
@@ -1,9 +1,19 @@
 const gs1 = require('../services/gs1Service');
 
 const getProduct = (req, res) => {
-  console.log(req.query.code);
   gs1.getProduct(req.query.code).then((data) => {
-    res.json(data);
+    const dto = data.ResponseDTO.ResponseItems[0];
+    const resp = {
+                  "descricao": dto.Descricao,
+                  "razao_social": dto.RazaoSocial,
+                  "marca": dto.MarcaProduto,
+                  "code": dto.Gtin,
+                  "largura": dto.Largura,
+                  "peso_liquido" : dto.PesoLiquido,
+                  "altura": dto.Altura,
+                  "peso_bruto": dto.PesoBruto
+                }
+    res.json(resp);
   });
 }
 
