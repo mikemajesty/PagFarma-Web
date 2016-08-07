27746234f83d3787a9532efcf083c6e2e961bed5
mikemajesty <mikemajesty2011@gmail.com>
2016-08-07 04:50:26 -0300

    adjusts in databse

--- server/router/gs1Router.js
+++ server/router/gs1Router.js
@@ -3,27 +3,35 @@ const productService = require('../services/productService');
 
 const getProduct = (req, res) => {
   const code = req.query.code;
-  gs1.getProduct(code).then((data) => {
-    if (data) {
-      const dto = data.ResponseDTO.ResponseItems[0];
-      const resp = {
-                    "descricao": dto.Descricao,
-                    "razao_social": dto.RazaoSocial,
-                    "marca": dto.MarcaProduto,
-                    "code": dto.Gtin,
-                    "largura": dto.Largura,
-                    "peso_liquido" : dto.PesoLiquido,
-                    "altura": dto.Altura,
-                    "peso_bruto": dto.PesoBruto
-                  }
-      productService.create(resp);
-      res.json(resp);
-    }
-  }).catch((err) => {
-    productService.findOne({code: code}).then((data) => {
-      res.json(data.get());
-    });
+  productService.findOne({code: code}).then((data) => {
+    res.json(data.get());
   });
+  // gs1.getProduct(code).then((data) => {
+  //   if (data) {
+  //
+  //     const dto = data.ResponseDTO.ResponseItems[0];
+  //     const resp = {
+  //                   "descricao": dto.Descricao,
+  //                   "razao_social": dto.RazaoSocial,
+  //                   "marca": dto.MarcaProduto,
+  //                   "code": dto.Gtin,
+  //                   "largura": dto.Largura,
+  //                   "peso_liquido" : dto.PesoLiquido,
+  //                   "altura": dto.Altura,
+  //                   "peso_bruto": dto.PesoBruto
+  //                 }
+  //
+  //
+  //     productService.create(resp);
+  //     res.json(resp);
+  //   }
+  //   res.json(200);
+  // }).catch((err) => {
+  //   console.log('wtf', err);
+  //   productService.findOne({code: code}).then((data) => {
+  //     res.json(data.get());
+  //   });
+  // });
 }
 
 module.exports = {
