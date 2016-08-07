9bc920ef6c01634a13933ddf9e5628990ae0c5f5
mikemajesty <mikemajesty2011@gmail.com>
2016-08-07 06:30:36 -0300

    fix bug

--- server/router/solicitationRouter.js
+++ server/router/solicitationRouter.js
@@ -11,7 +11,7 @@ const findAll = (req, res) => {
   const userId = req.query.userId;
   return solicitationService.findAll({userId: userId})
     .then((data) => {
-      res.sendStatus(data);
+      res.json(data);
     }).catch((err) => {
       res.sendStatus(500);
     });
@@ -21,7 +21,7 @@ const findAllBudget = (req, res) => {
   const status = req.query.status;
   return solicitationService.findAllBudget({status: status})
     .then((data) => {
-      res.sendStatus(data);
+      res.json(data);
     }).catch((err) => {
       console.log(err);
       res.sendStatus(500);
