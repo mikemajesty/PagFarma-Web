f88a58828b4849de1a7db64f14064f343e504f72
mikemajesty <mikemajesty2011@gmail.com>
2016-08-07 07:10:31 -0300

    fix bug

--- server/model/budget.js
+++ server/model/budget.js
@@ -4,6 +4,7 @@ const Farma = require('./farma').Farma;
 
 const Budget = sequelize.define('budget', {
   value: Sequelize.DECIMAL,
+  name: {type: Sequelize.STRING, defaultValue: "Drogasil"},
   status: {type: Sequelize.STRING, defaultValue: "NOT APROVED"}
 });
 
