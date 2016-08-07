e17a1b1d3e52be899697e5f137981f9e2f3db5ae
mikemajesty <mikemajesty2011@gmail.com>
2016-08-07 05:14:01 -0300

    fix bug

--- server/server.js
+++ server/server.js
@@ -21,6 +21,24 @@ app.post('/user/create', userRouter.create);
 app.post('/farma/create', farmaRouter.create);
 app.post('/farma/login', farmaRouter.login);
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
 // SOLICITATION
 app.post('/order/create', solicitationRouter.create);
 app.get('/order/', solicitationRouter.findAll);
